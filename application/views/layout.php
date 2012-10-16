2e3aa4b8229699fb91b686d4d12a2ec9eee68e81
xavier Bekaert <xavier24@hotmail.com>
2012-10-16 10:08:48 +0200

    routage+reecriture url

---
+++ application/views/layout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr-BE">
+<head>
+	<meta charset="utf-8">
+	<title>Bienvenue sur Adopte un prof - <?php echo $titre; ?></title>
+</head>
+<body>
+    <?php echo $vue ?>
+
+</body>
+</html>
---
+++ application/views/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bienvenue sur Adopte un prof</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Je suis dans test</h1>
+
+	
+</div>
+
+</body>
+</html>
